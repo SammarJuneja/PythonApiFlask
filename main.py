880cd11d1aebe540924f436623eccacd23c2c44f
SammarJuneja <sammarkumar345@gmail.com>
2024-06-10 13:16:31 +0530

    Added mongodb

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import os
-import pymongo
+from pymongo import MongoClient
+
+client = MongoClient()
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.get("/")
+def route():
+    return "<h1>This is the Bank api made with python </h1>"
+    
+@app.get("/test/:user")
+def test():
+    return f"<h2>{user}</h2>"
