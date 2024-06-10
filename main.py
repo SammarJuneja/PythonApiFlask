39d1bbc220a0caa3f3a01b06e6e5036318019d71
SammarJuneja <sammarkumar345@gmail.com>
2024-06-10 18:00:51 +0530

    MomgoDB setup

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import os
-from pymongo import MongoClient
-
-client = MongoClient()
-
 from flask import Flask
+from pymongo import MongoClient, errors
+from dotenv import load_dotenv
+
 app = Flask(__name__)
+load_dotenv()
+
+MONGO = os.getenv("MONGO_URI")
+client = MongoClient(MONGO)
+db = client.get_database()
 
 @app.get("/")
 def route():
     return "<h1>This is the Bank api made with python </h1>"
     
-@app.get("/test/:user")
-def test():
+@app.get("/test/<user>")
+def test(user):
     return f"<h2>{user}</h2>"
+    
+if __name__ == "__main__":
+  app.run(debug=True)
