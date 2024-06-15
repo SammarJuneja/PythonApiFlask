1ce07fdf7141d5ab3864dc402fafb831540117ef
SammarJuneja <sammarkumar345@gmail.com>
2024-06-15 19:49:28 +0530

    Added conditions and regex in createUser endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from pymongo.errors import OperationFailure
@@ -13,20 +14,35 @@ db = client["BankApi"]
 
 @app.get("/")
 def root():
-    return "<h1>This is the Bank api made with python </h1>"
+    return "<h1>This is the Bank api made with python</h1>"
     
 @app.post("/create-user")
 def createUser():
    data = request.get_json()
    username = data["username"]
+   email = data["email"]
+   password = data["password"]
+
+   emailRegex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
+   passwordRegex = r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+
    if not data or "username" not in data:
       return jsonify({ "error": "Userame is missing" }), 404
+   elif not data or "email" not in data:
+      return jsonify({ "error": "Email is missing" }), 404
+   elif not data or "password" not in data:
+      return jsonify({ "error": "Password is missing" }), 404
    
-   try:
-      db.users.insert_one(data)
-      return jsonify({ "message": f"Your bank account is created with username {username}"})
-   except OperationFailure as e:
-      return jsonify({ "message": f"Failed to create account {e}"})
+   if re.match(passwordRegex, password):
+      return jsonify({ "error": "Password must atleast be 8 characters long and should contain One uppercase ltter and a symbol" })
+   elif re.match(emailRegex, email):
+      return jsonify({ "error": "Please enter a valid email" })
+   else:
+      try:
+         db.users.insert_one({ username: username, email: email, password: password })
+         return jsonify({ "message": f"Your bank account is created with username {username}"})
+      except OperationFailure as e:
+         return jsonify({ "message": f"Failed to create account {e}"})
     
 if __name__ == "__main__":
   app.run(debug=True)
