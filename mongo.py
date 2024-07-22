684c84e2f9c204e9217c429b90018844d28555a5
Sammar Juneja <70101180+SammarJuneja@users.noreply.github.com>
2024-07-22 15:44:43 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,1 +1,9 @@
-m
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO = os.getenv("MONGO_URI")
+JWT_SECRET = os.getenv("SECRET_KEY")
+client = MongoClient(MONGO)
+db = client["BankApi"]
