f5e334e47cfb0612a5d1d1c23d623c0401fd2ccf
Sammar Juneja <70101180+SammarJuneja@users.noreply.github.com>
2024-07-23 15:23:01 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,9 +1,10 @@
-import os
+#import os
 from pymongo import MongoClient
+import config from config
 from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO = os.getenv("MONGO_URI")
-client = MongoClient(MONGO)
+#MONGO = os.getenv("MONGO_URI")
+client = MongoClient(config.MONGO)
 db = client["BankApi"]
